24a13df09c2634b1d97d550701d2ed30e80d874b
Arie <arieinjector@gmail.com>
2023-02-16 21:42:28 +0700

    add seo

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <meta name="description" content="Selamat Datang Di Website Kisi-Kisi Sekolah Dan Selamat Belajar, Di website ini anda bisa mengakses kisi-kisi PAS yang ada di SMK Parakacuk," />
+        <meta name="keywords" content="kedok kisi, ariear, kedok-kisi, kisi kisi github, kisi-kisi sekolah, panduan siswa, jenis soal, format tes, sumber daya pendukung" />
+        <meta name="robots" content="index,follow"></meta>
+      </Head>
       <body className='bg-[#2B3A55]'>
         <Main />
         <NextScript />
